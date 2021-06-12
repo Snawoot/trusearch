2313721496f94d76b58376b5922b94f6f470cef3
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-06-12 16:07:28 +0300

    CLI is mostly completed

--- cli/main.go
+++ cli/main.go
@@ -13,6 +13,7 @@ var CLI struct {
 		ScriptFile *os.File   `arg help:"JS script executed for each torrent object"`
 		Xmls       []*os.File `arg help:"XML files to process"`
 	} `cmd help:"Scan XML and apply JS function defined by script file"`
+	Help struct {} `cmd default:"1" help:"Prints CLI synopsis"`
 }
 
 func main() {
@@ -20,6 +21,15 @@ func main() {
 	switch ctx.Command() {
 	case "scan <script-file> <xmls>":
 		fmt.Printf("%#v", CLI.Scan)
+	case "help":
+		parser, err := kong.New(&CLI)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = parser.Parse([]string{"--help"})
+		if err != nil {
+			log.Fatal(err)
+		}
 	default:
 		log.Fatal("Unknown command:", ctx.Command())
 	}
