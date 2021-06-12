79a9133e7445afde1eaa2cb7baef455b4989e86b
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-06-12 15:27:50 +0300

    kong

--- cli/main.go
+++ cli/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/alecthomas/kong"
 )
 
+var CLI struct {
+	Scan struct {
+		ScriptFile *os.File   `arg help:"JS script executed for each torrent object"`
+		Xmls       []*os.File `arg help:"XML files to process"`
+	} `cmd help:"Scan XML and apply JS function defined by script file"`
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	ctx := kong.Parse(&CLI)
+	switch ctx.Command() {
+	case "scan <script-file> <xmls>":
+		fmt.Printf("%#v", CLI.Scan)
+	default:
+		log.Fatal("Unknown command:", ctx.Command())
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Snawoot/trusearch
 
 go 1.16
+
+require github.com/alecthomas/kong v0.2.17
