71ab833594f10798ed777b9a816253bf5c1c4ceb
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-06-12 20:39:04 +0300

    input scanning operational

--- task/forums/forums.go
+++ task/forums/forums.go
@@ -1,9 +1,24 @@
 package forums
 
 import (
+	"io"
+	"log"
+
 	"github.com/Snawoot/trusearch/def"
 )
 
-func Forums(xmldata def.RecordScanner) int {
+func Forums(scanner def.RecordScanner) int {
+	for {
+		rec, err := scanner.Scan()
+		if err == io.EOF {
+			return 0
+		}
+		if err != nil {
+			log.Printf("Got error on input scan: %v", err)
+			return 3
+		}
+
+		log.Printf("%#v", rec)
+	}
 	return 0
 }
