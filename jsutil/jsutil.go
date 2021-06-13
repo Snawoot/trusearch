e2054e8d3ba8e0e0fd2dbe3e732ee611f321197b
Vladislav Yarmak <vladislav-ex-src@vm-0.com>
2021-06-13 17:34:37 +0300

    js: add begin/end hooks

---
+++ jsutil/jsutil.go
@@ -0,0 +1,25 @@
+package jsutil
+
+import (
+	"errors"
+
+	"github.com/dop251/goja"
+)
+
+var (
+	ErrUndefined = errors.New("value is not defined")
+	ErrNotAFunction = errors.New("value is not a function")
+)
+
+func FnInvoke(vm *goja.Runtime, name string, args ...goja.Value) (goja.Value, error) {
+	val := vm.Get(name)
+	if val == nil {
+		return nil, ErrUndefined
+	}
+	fn, ok := goja.AssertFunction(val)
+	if !ok {
+		return nil, ErrNotAFunction
+	}
+
+	return fn(goja.Undefined(), args...)
+}
